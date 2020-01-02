34357c25dfd0e15c4617d773fd1a7c59de96f513
bhkj9999 <40259944+bhkj9999@users.noreply.github.com>
2020-01-02 00:09:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# warmane
+ getEventInformationOfWarmane
---
+++ warmane_notification.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import hashlib
+
+def getRes(url):
+    res = requests.get(url)
+    
+    formattedRes = BeautifulSoup(res.text, 'html.parser')
+
+    return formattedRes
+
+def getAllPelement():
+    url = 'https://www.warmane.com/'
+
+    text = getRes(url)
+
+    all_P_Tag = text.findAll('p')
+
+    filter_A = 'href'
+
+    result = []
+
+    for i in all_P_Tag:
+        if(filter_A in str(i)):
+            result.append(str(i))
+
+    return result
+
+def printElement():
+    final = getAllPelement()
+
+    print(final)
+
+    return final
+
+def md5():
+    final = getAllPelement()
+
+    md5 = hashlib.md5()
+
+    md5.update(str(final).encode('utf-8'))
+
+    print(md5.hexdigest())
+    
+    return md5.hexdigest()
