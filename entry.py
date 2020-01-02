2d6feb307f49de1e43213e32bf4ab7d37f013ac1
henryZ <czhang39@nyit.edu>
2020-01-02 18:48:15 -0500

    update logic

--- entry.py
+++ entry.py
@@ -2,7 +2,7 @@
 @Author: Henry Zhang
 @Date: 2020-01-02 15:01:05
 @LastEditors  : Henry Zhang
-@LastEditTime : 2020-01-02 17:26:16
+@LastEditTime : 2020-01-02 18:17:54
 @Github: https://github.com/bhkj9999
 '''
 
@@ -24,7 +24,7 @@ def saveToFile():
         # file.write(md5 + Latest_Event + "Time of LA" + time.ctime() + "\n" )
         # file.close()
     else:
-        auto_send_em.sendInfo("Nothing Changed \n" + "The Latest Event is " + Latest_Event)
+        auto_send_em.sendInfo("Nothing Changed \n\n" + "MD5 Check \n" + md5 + "\n" + "The Latest Event is \n" + Latest_Event)
         # file.write("md5 and Latest_Event Didn't Change on Time of LA" + time.ctime() + "\n" )
         # file.close()
     
