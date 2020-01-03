a3bf5d26b6bd5f9a1a446f211ae1d2895c64b70f
bhkj9999 <40259944+bhkj9999@users.noreply.github.com>
2020-01-02 21:48:17 -0500

    delete useless

--- recordExample.py
+++ recordExample.py
@@ -1,9 +1,1 @@
-'''
-@Author: Henry Zhang
-@Date: 2020-01-02 15:37:08
-@LastEditors  : Henry Zhang
-@LastEditTime : 2020-01-02 15:37:42
-@Github: https://github.com/bhkj9999
-'''
-md5 = "54f2dd231ca36aac8d1ee647b809fb12"
-content = '<p>SoloQueue is now available on all of our WoTLK realms. More information can be found <a href="http://forum.warmane.com/showthread.php?t=411778">here</a>.</p>'
+md5 = "2945a03355ec5dc8a344f58d7897ecf9"
