a5932b0a733814faefd8815aa443c26d2422d405
bhkj9999 <40259944+bhkj9999@users.noreply.github.com>
2020-01-04 00:48:00 -0500

    error handle

--- entry.py
+++ entry.py
@@ -9,37 +9,32 @@
 import warmane_notification
 import auto_send_em
 import recordExample
-import os, time, platform
-
+from Receiver import receiver
+from resources import url
 
 def saveToFile():
-    url = 'https://www.warmane.com/'
     
     md5 = warmane_notification.md5(url)
     Latest_Event_List = warmane_notification.getAllPelement(url)
     Latest_Event = Latest_Event_List[0] if Latest_Event_List else 'Error'
 
-    location = os.getcwd()
-    sysstr = platform.system()
-
-    if(sysstr == "Windows"):
-        filePath = open(location + r"\recordExample.py", 'r+')
-        errorFilePath = open(location + r"\error.txt", 'r+')
-    else:
-        filePath = open(location + "/recordExample.py", 'r+')
-        errorFilePath = open(location + "/error.txt", 'r+')
-
     request_status_code = warmane_notification.getFormattedRes(url)[1]
+
+    filePath = auto_send_em.openFile()[0]
+    errorFilePath = auto_send_em.openFile()[1]
 
     if( request_status_code == 200 ):
         if( recordExample.md5 != md5 ):
-            auto_send_em.sendInfo("Latest Event Found \n\n" + "MD5 Check \n" + md5 + "\n\n" + "The Latest Event is \n" + Latest_Event)
+            auto_send_em.sendInfo("Latest Event Found \n\n" + "MD5 Check \n" + md5 + "\n\n" + "The Latest Event is \n" + Latest_Event, receiver)
             print("Latest Event Found \n\n" + "MD5 Check \n" + md5 + "\n\n" + "The Latest Event is \n" + Latest_Event)
             filePath.write("md5 = " + r'"' + md5 + r'"')
         else:
             print("Nothing Changed \n\n" + "MD5 Check \n" + md5 + "\n" + "\n" + "The Latest Event is \n" + Latest_Event)
     else:
-        errorFilePath.write('Request Error ' + str(request_status_code))
+        if(auto_send_em.sendInfo()[1]):
+            errorFilePath.write('Gmail Error ' + str(auto_send_em.sendInfo()[1]) + "Request Info " + str(request_status_code))
+        else:
+            errorFilePath.write('Request Error ' + str(request_status_code))
 
     filePath.close()
     errorFilePath.close()
